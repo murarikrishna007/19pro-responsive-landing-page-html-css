1ea44c5af44507b18ee262fd76811488216cf38a
murarikrishna007 <murarikrishna0071994@gmail.com>
2021-12-06 00:24:35 +0530

    responsive landing page website build in html and css

---
+++ script.js
@@ -0,0 +1,8 @@
+// console.log("hello krishna murari");
+
+const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
+const navMenu = document.getElementsByClassName("nav-menu")[0];
+
+toggleBtn.addEventListener("click",()=>{
+    navMenu.classList.toggle("active")
+});
